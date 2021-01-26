c53957278afb51f71597076537c6810773179438
u1225394@eng.utah.edu <u1225394@eng.utah.edu>
2021-01-26 15:03:34 -0700

    Added method stubs, delegates, etc. as provided in PS1 specifications.

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -2,8 +2,37 @@
 
 namespace FormulaEvaluator
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class Evaluator
     {
-        //to-do: follow PS1 instructions
+        public delegate int Lookup(String v);
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="variableEvaluator"></param>
+        /// <returns></returns>
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            //to-do
+
+
+
+            //to-do: repurpose code, borrowed from assignment description, divides string into tokens
+            //string[] substrings = Regex.Split(s, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            return 0;
+        }
+
+
+
+
+
+
     }
 }
