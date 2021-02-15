0eccb9c985bca1e7a80dee3617e37e119e9edda4
u1225394@eng.utah.edu <u1225394@eng.utah.edu>
2021-02-14 21:53:17 -0700

    PS4: Initial project set-up. Essential class files / references are in place.

---
+++ Spreadsheet/Spreadsheet/Cell.cs
@@ -0,0 +1,20 @@
+﻿// Author: David Clark
+// CS 3500
+// February 2021
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpreadsheetUtilities
+{
+    class Cell
+    {
+
+        public Cell()
+        {
+
+        }
+
+    }
+}
---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,49 @@
+﻿// Author: David Clark
+// CS 3500
+// February 2021
+
+using SpreadsheetUtilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SS
+{
+    class Spreadsheet : AbstractSpreadsheet
+    {
+        public Spreadsheet()
+        {
+
+        }
+
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
