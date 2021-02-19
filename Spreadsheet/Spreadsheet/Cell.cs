88a17044e94ecf8b27b4ec6048c3aa522d2134e9
u1225394@eng.utah.edu <u1225394@eng.utah.edu>
2021-02-18 21:13:33 -0700

    PS4: the Spreadsheet class is now fully coded/functional. It passes all tests. It still needs comments / cleanup. Additional testing is needed.

--- Spreadsheet/Spreadsheet/Cell.cs
+++ Spreadsheet/Spreadsheet/Cell.cs
@@ -8,13 +8,92 @@ using System.Text;
 
 namespace SpreadsheetUtilities
 {
-    class Cell
+
+    /// <summary>
+    /// A Cell stores its contents (either a Double, String, or Formula)
+    /// and its value (either a Double, String, or FormulaError).
+    /// </summary>
+    internal class Cell
     {
+        // backend values  
+        private object p_contents;
+        private object p_value;
 
-        public Cell()
+        // helps to identify the cell's contents
+        private bool isDouble = false;
+        private bool isString = false;
+        private bool isFormula = false;
+
+        /// <summary>
+        /// Sets the Cell's contents to the provided double.
+        /// </summary>
+        public Cell(double content)
         {
-
+            this.isDouble = true;
+            p_contents = content;
         }
+
+        public Cell(string content)
+        {
+            this.isString = true;
+            p_contents = content;
+        }
+
+        public Cell(Formula content)
+        {
+            this.isFormula = true;
+            p_contents = content;
+        }
+
+        
+
+
+        public object Contents
+        {
+            get
+            {
+                if (this.isDouble)
+                    return (double)p_contents;
+                else if (this.isString)
+                    return (string)p_contents;
+                else if (this.isFormula)
+                    return (Formula)p_contents;
+                else
+                    throw new Exception("Invalid state. Cell contents are not marked " +
+                        "as containing a double, string, or formula: cannot be evaluated.");
+            }
+
+            set
+            {
+                // if value is an invalid type, return without modifying this cell
+                if (!((value is Double) || (value is String) || (value is Formula)))
+                    return;
+
+                // clear the cell's previous state
+                this.isDouble = false;
+                this.isString = false;
+                this.isFormula = false;
+                p_contents = null;
+
+                // set the cell's new data
+                if (value is Double)
+                {
+                    this.isDouble = true;
+                    p_contents = value;
+                }
+                else if (value is String)
+                {
+                    this.isString = true;
+                    p_contents = value;
+                }
+                else if (value is Formula)
+                {
+                    this.isFormula = true;
+                    p_contents = value;
+                }
+            }
+        }
+
 
     }
 }
--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -9,41 +9,190 @@ using System.Text;
 
 namespace SS
 {
-    class Spreadsheet : AbstractSpreadsheet
+    public class Spreadsheet : AbstractSpreadsheet
     {
+        // 
+        private DependencyGraph depGraph;
+        private Dictionary<string, Cell> nonemptyCellsDict;
+
         public Spreadsheet()
         {
+            depGraph = new DependencyGraph();
+            nonemptyCellsDict = new Dictionary<string, Cell>();
+        }
+
+        /// <summary>
+        /// Returns true if "name" is valid as determined by the Formula class.
+        /// </summary>
+        /// <param name="name">a potential cell name</param>
+        private bool isValidCellName(string name)
+        {
+            // Create a new Formula with the name. If Formula throws, the name was rejected, i.e. it is not a valid variable name.
+            try
+            {
+                new Formula(name);
+                return true;
+            }
+            catch (FormulaFormatException e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Throws an InvalidNameException if...
+        /// ...a cell name is null
+        /// ...a cell name doesn't match the Formula specifications for a variable
+        /// </summary>
+        /// <param name="name"></param>
+        private void throwIfInvalidName(string name)
+        {
+            if (ReferenceEquals(name, null))
+                throw new InvalidNameException();
+
+            if (!isValidCellName(name))
+                throw new InvalidNameException();
+        }
+
+        /// <summary>
+        /// Helper for SetCellContents(Formula).
+        /// Throws a CircularException if changing this Cell's value to the given formula
+        /// would result in a cycle.
+        /// </summary>
+        /// <param name="name">the name of a valid cell in this spreadsheet</param>
+        /// <param name="formula">the formula that would be used as the Cell's new value</param>
+        private void throwIfCircularException(string name, Formula formula)
+        {
+            // enumerate through each dependent of the cell tagged as "name"
+            IEnumerator<string> dependentsEnum = GetCellsToRecalculate(name).GetEnumerator();
+
+            // skip the first element, which should be the original cell itself
+            dependentsEnum.MoveNext();
+
+            while (dependentsEnum.MoveNext())
+            {
+                string currDependent = dependentsEnum.Current;
+
+                // if the currDependent cell is referenced in the formula, then the cell would depend on its own dependents
+                // --> throw exception
+                foreach (string var in formula.GetVariables())
+                {
+                    if (var == currDependent)
+                        throw new CircularException();
+                }
+
+            }
+
 
         }
 
         public override object GetCellContents(string name)
         {
-            throw new NotImplementedException();
+            // throws an InvalidNameException if necessary
+            throwIfInvalidName(name);
+
+            // if cell is not empty
+            if (nonemptyCellsDict.TryGetValue(name, out Cell resultCell))
+            {
+                return resultCell.Contents;
+            }
+            else
+            {
+                return "";
+            }
+
         }
 
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
-            throw new NotImplementedException();
+            return nonemptyCellsDict.Keys;
+        }
+
+        /// <summary>
+        /// Helper for SetCellContents().
+        /// Returns a list of a Cell and all of its direct/indirect dependents.
+        /// Based on GetCellsToRecalculate().
+        /// </summary>
+        /// <param name="name">the name of a cell in this spreadsheet</param>
+        private IList<string> GetListToRecalculate(string name)
+        {
+            IList<string> listToRecalc = new List<string>();
+            foreach (string tempCellName in GetCellsToRecalculate(name))
+            {
+                listToRecalc.Add(tempCellName);
+            }
+
+            return listToRecalc;
         }
 
         public override IList<string> SetCellContents(string name, double number)
         {
-            throw new NotImplementedException();
+            // throws exceptions if necessary
+            throwIfInvalidName(name);
+
+            // replaces the Cell's contents, or creates a new cell
+            if (nonemptyCellsDict.TryGetValue(name, out Cell cellToReset))
+                cellToReset.Contents = number;
+            else
+                nonemptyCellsDict.Add(name, new Cell(number));
+
+            // returns the list of self + direct/indirect dependents
+            return GetListToRecalculate(name);
         }
 
         public override IList<string> SetCellContents(string name, string text)
         {
-            throw new NotImplementedException();
+            // throws exceptions if necessary
+            throwIfInvalidName(name);
+            if (ReferenceEquals(text, null))
+                throw new ArgumentNullException();
+
+            // replaces the Cell's contents, or creates a new cell
+            if (nonemptyCellsDict.TryGetValue(name, out Cell cellToReset))
+            {
+                cellToReset.Contents = text;
+
+                // if the cell was set to the empty string, remove it from the dictionary
+                if (text == "")
+                    nonemptyCellsDict.Remove(name);
+            }
+            else
+            {
+                nonemptyCellsDict.Add(name, new Cell(text));
+            }
+
+            // returns the list of self + direct/indirect dependents
+            return GetListToRecalculate(name);
         }
 
         public override IList<string> SetCellContents(string name, Formula formula)
         {
-            throw new NotImplementedException();
+            // throws exceptions if necessary
+            throwIfInvalidName(name);
+
+            if (ReferenceEquals(formula, null))
+                throw new ArgumentNullException();
+            
+            throwIfCircularException(name, formula);
+
+
+            // replaces the Cell's contents, or creates a new cell
+            if (nonemptyCellsDict.TryGetValue(name, out Cell cellToReset))
+                cellToReset.Contents = formula;
+            else
+                nonemptyCellsDict.Add(name, new Cell(formula));
+
+            // updates the Cell's dependencees
+            depGraph.ReplaceDependees(name, formula.GetVariables());
+
+
+            // returns the list of self + direct/indirect dependents
+            return GetListToRecalculate(name);
         }
 
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
-            throw new NotImplementedException();
+            return depGraph.GetDependents(name);
         }
     }
 }
